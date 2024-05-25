82e2adf08c97b5ffa2840c6fb33d4669cf878928
Manav <manav18gadhiya@gmail.com>
2024-05-25 19:23:25 +0530

    add:✅ getHighestAPY API

---
+++ packages/nextjs/app/api/getHighestAPY/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    try {
+        const response = await fetch('https://yields.llama.fi/pools', {cache: "no-cache"});
+        const dataObject = await response.json();
+        const dataArray = dataObject.data;
+        const apyArray = [];
+
+        for (let i = 0; i < dataArray.length; i++) {
+            const element = dataArray[i];
+            
+            if (
+                (element.chain === "Ethereum" || element.chain === "Sepolia" || element.chain === "Polygon") &&
+                (element.symbol === "USDC" || element.symbol === "ETH") &&
+                (element.project === "aave-v3" || element.project === "compound-v3")
+            ) {
+                apyArray.push(element.apy);
+            }
+        }
+
+        const highestAPY = Math.max(...apyArray);
+        return new NextResponse(JSON.stringify(highestAPY), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new NextResponse("There was an Error", { status: 500 });
+    }
+}
