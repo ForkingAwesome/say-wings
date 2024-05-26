3bade007d8abae270f9df45886ba16e5f02834a4
Manav <manav18gadhiya@gmail.com>
2024-05-26 07:00:58 +0530

    add:✅ Mint Receipt NFT

--- packages/nextjs/utils/addresses.js
+++ packages/nextjs/utils/addresses.js
@@ -1,4 +1,5 @@
 export const addresses = {
   "CompoundContractAddress": "0xDa4Cb28C7a348B3B83052613294f5353a1695e5a",
-  "USDCCompoundContract": "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+  "USDCCompoundContract": "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
+  "SepoliaNftAddress": "0xAc5EE7BBCCc39A5872b73e443f8893425DE3EdF6"
 }
